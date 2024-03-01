168ac71b3fb7243f786d1c9797fbca45aac0a350
SethTooSaucy <111447287+SethTooSaucy@users.noreply.github.com>
2024-03-01 14:28:53 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://lillaundry:<Antib7iotics!>@sethcluster.lbpora8.mongodb.net/?retryWrites=true&w=majority&appName=SethCluster";
+
+
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -35,8 +37,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('Sethdb');
+    const parts = database.collection('SethStuff');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
@@ -54,3 +56,4 @@ async function run() {
 }
 run().catch(console.dir);
 });
+
