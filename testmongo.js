0e790becc4653e9e5a26b28c652ea1bf9b793172
SethTooSaucy <111447287+SethTooSaucy@users.noreply.github.com>
2024-03-04 21:28:57 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,9 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://lillaundry:<Antib7iotics!>@sethcluster.lbpora8.mongodb.net/?retryWrites=true&w=majority&appName=SethCluster";
-
-
+const uri = "mongodb+srv://lillaundry:Antib7iotics!@sethcluster.lbpora8.mongodb.net/?retryWrites=true&w=majority&appName=SethCluster";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -14,8 +12,6 @@ console.log('Server started at http://localhost:' + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// routes will go here
 
 // Default route:
 app.get('/', function(req, res) {
@@ -28,31 +24,33 @@ app.get('/say/:name', function(req, res) {
   res.send('Hello ' + req.params.name + '!');
 });
 
-
 // Route to access database:
 app.get('/api/mongo/:item', function(req, res) {
-const client = new MongoClient(uri);
-const searchKey = "{ partID: '" + req.params.item + "' }";
-console.log("Looking for: " + searchKey);
+  const client = new MongoClient(uri);
+  const searchKey = "{ partID: '" + req.params.item + "' }";
+  console.log("Looking for: " + searchKey);
 
-async function run() {
-  try {
-    const database = client.db('Sethdb');
-    const parts = database.collection('SethStuff');
+  async function run() {
+    try {
+      const database = client.db('Sethdb');
+      const parts = database.collection('SethStuff');
 
-    // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
-    // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+      // Hardwired Query for a part that has partID '12345'
+      // const query = { partID: '12345' };
+      // But we will use the parameter provided with the route
+      const query = { partID: req.params.item };
 
-    const part = await parts.findOne(query);
-    console.log(part);
-    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
+      const part = await parts.findOne(query);
+      console.log(part);
+      res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
   }
+  run().catch(console.dir);
+});
 }
 run().catch(console.dir);
 });
